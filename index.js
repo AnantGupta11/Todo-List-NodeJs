23e687a1fdfd1f4d6ab3188dd6d0e71b45dfc0f0
Anant8002749 <ak8002749@gmail.com>
2020-06-12 22:12:37 +0530

    Setup express Server

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-const http= require('http');
-//port no where the server running
+const express = require('express');
+
 const port =8000;
+// for firing up express server
+const app= express();
 
-//fs module is a node js module which helps in reading and writing files
-const fs =require('fs');
 
-//this function handle the server req and respose
-function requestHandler(req, res){
-    console.log(req.url);
-    //writeHead is use to tell the server which type of data send
-    res.writeHead(200,{'content-type': 'text/html'});
-    fs.readFile('./index.html', function(err,data){
-        if(err){
-            console.log('Error in load file',err);
-            return res.end('<h1>Error</h1>');
-        }
-        return res.end(data);
 
-    })
-    //return res.end('<h1>Hello</h1>');
-}
 
-//method in http for creating server
-const server = http.createServer(requestHandler);
 
-//for running the server server should listen the port
 
-server.listen(port,function(err){
+
+app.get('/', function(req, res){
+    res.send('<h1>Anant</h1>');
+})
+
+
+
+app.listen(port, function(err){
     if(err){
-        console.log(err);
-        return;
+        console.log('Error in running in server', err);
+        
     }
-    console.log("Server is running on port:",port);
+    console.log('Server is running on port:',port);
+
 });
