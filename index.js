c972e6beba1d38fd453de8f052d4b9c113fa47fe
Anant8002749 <ak8002749@gmail.com>
2020-06-12 13:28:00 +0530

    Added file System

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const http= require('http');
 //port no where the server running
-const port =800;
+const port =8000;
+
+//fs module is a node js module which helps in reading and writing files
+const fs =require('fs');
 
 //this function handle the server req and respose
 function requestHandler(req, res){
     console.log(req.url);
-    return res.end('<h1>Hello</h1>');
+    //writeHead is use to tell the server which type of data send
+    res.writeHead(200,{'content-type': 'text/html'});
+    fs.readFile('./index.html', function(err,data){
+        if(err){
+            console.log('Error in load file',err);
+            return res.end('<h1>Error</h1>');
+        }
+        return res.end(data);
+
+    })
+    //return res.end('<h1>Hello</h1>');
 }
 
 //method in http for creating server
@@ -18,5 +31,5 @@ server.listen(port,function(err){
         console.log(err);
         return;
     }
-    console.log("Server is running on port: 80")
+    console.log("Server is running on port:",port);
 });
